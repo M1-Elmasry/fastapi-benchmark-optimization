3b28ae0df97fa76fdb419b6a9fd49d411f690e7a
M1-Elmasry <mostafamfelmasry@outlook.com>
2025-04-03 08:59:57 +0200

    update test_servers script

--- scripts/test_servers.sh
+++ scripts/test_servers.sh
@@ -24,15 +24,15 @@ run_tests ()
 run_servers () {
   local server_type=$1 # sync or async
   local log_file_name_extension=$2 # optional
-  local log_file="logs/${server_type}_server_{$log_file_name_extension}.log"
+  local log_file="logs/${server_type}_server_${log_file_name_extension}.log"
   echo "------------start ${server_type} server------------"
-  uvicorn servers.${server_type}_server:app --host 127.0.0.1 --port 3000 > log_file 2>&1 &
+  uvicorn servers.${server_type}_server:app --host 127.0.0.1 --port 3000 > $log_file 2>&1 &
   SYNC_SERVER_PID=$!
 
   sleep 5 # wait for the server to start
 
   echo -e "\n------------running ${test} tests------------ $(date)\n"
-  run_tests ${server_type}_server_{$log_file_name_extension}_tests.log
+  run_tests ${server_type}_server_${log_file_name_extension}_tests.log
   echo -e "\n------------tests finished------------ $(date)\n"
 
   echo -e "\n------------stop ${server_type} server------------\n"
@@ -46,11 +46,10 @@ echo -e "make sure you activate the venv: source .venv/bin/activate\n"
 
 if [ ! -d "logs" ]; then mkdir logs; fi
 
-# first use
-# run_servers "sync"
-# run_servers "async"
+run_servers "async"
 
-# second use
+uv add httptools uvloop
+
 run_servers "async" "httptools_uvloop"
 
 
